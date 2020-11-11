ec4b926c92192f4be874960501b5643668344c35
zz132456zz <zz132456zz@kakao.com>
2020-11-11 17:54:23 +0900

    parse HTML page with jsoup

--- Reservation_Notification/app/src/main/java/com/example/reservation_notification/MainActivity.java
+++ Reservation_Notification/app/src/main/java/com/example/reservation_notification/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.reservation_notification;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView3);
+        editText = (EditText) findViewById(R.id.editTextTextPersonName);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWeb();
+            }
+        });
+
     }
+
+    private void getWeb(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final StringBuilder builder = new StringBuilder();
+
+                try {
+                    Document doc = Jsoup.connect("https://www.naver.com").get();
+                    String title = doc.title();
+
+                    builder.append(title);
+                } catch (IOException e){
+                    builder.append("Error");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(builder.toString());
+                    }
+                });
+
+            }
+        }).start();;
+    }
+
 }
